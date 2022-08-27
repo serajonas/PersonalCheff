5a2cee952747b55ba55932257454c0fb639dfab1
Jonas Serafim <104076718+serajonas@users.noreply.github.com>
2022-08-27 14:10:55 -0300

    Views

--- PersonalCheffProj/receitas/urls.py
+++ PersonalCheffProj/receitas/urls.py
@@ -1,7 +1,8 @@
 from operator import index
+from unicodedata import name
 from django.urls import path
 from.import views
 
 urlpatterns = [
-path('',views.index, name='index')
+path('', views.index, name='index')
 ]
--- PersonalCheffProj/receitas/views.py
+++ PersonalCheffProj/receitas/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse ("Sejam Bem Vindos")
+    
+        <img src="logo.png" alt="personalcheff">
 # Create your views here.
