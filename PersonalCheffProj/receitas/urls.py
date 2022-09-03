99f5ef0efffd60aff19dafa0e99c4f2c13f83b27
Jonas Serafim <104076718+serajonas@users.noreply.github.com>
2022-09-03 13:59:02 -0300

    Suco de Laranja

--- PersonalCheffProj/receitas/urls.py
+++ PersonalCheffProj/receitas/urls.py
@@ -1,10 +1,7 @@
-from operator import index
-from unicodedata import name
 from django.urls import path
 from.import views
 
 urlpatterns = [
 path('', views.index, name='index'),
-path('sucodelaranja',views.sucodelaranja
-name='sucodelaranja'),
+path('sucodelaranja',views.sucodelaranja,name='sucodelaranja'),
 ]
