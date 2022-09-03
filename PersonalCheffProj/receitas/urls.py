92a00466f4bd17752c60b616b6f0518d28fde0c8
Jonas Serafim <104076718+serajonas@users.noreply.github.com>
2022-09-03 13:52:46 -0300

    sucodelaranja

--- PersonalCheffProj/receitas/urls.py
+++ PersonalCheffProj/receitas/urls.py
@@ -4,5 +4,7 @@ from django.urls import path
 from.import views
 
 urlpatterns = [
-path('', views.index, name='index')
+path('', views.index, name='index'),
+path('sucodelaranja',views.sucodelaranja
+name='sucodelaranja'),
 ]
--- PersonalCheffProj/receitas/views.py
+++ PersonalCheffProj/receitas/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse ("Sejam Bem Vindos")
-    
-        <img src="logo.png" alt="personalcheff">
+    return render(request, 'index.html')
+def sucodelaranja(request):
+    return render(request,'sucodelaranja.html')    
+        
 # Create your views here.
