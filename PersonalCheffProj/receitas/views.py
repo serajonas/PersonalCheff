f5d73c3e1fbb824c9b2a1b55ed9f6dc1c8ab3fa8
Jonas Serafim <104076718+serajonas@users.noreply.github.com>
2022-09-24 13:21:13 -0300

    Aula 12

--- PersonalCheffProj/receitas/views.py
+++ PersonalCheffProj/receitas/views.py
@@ -1,7 +1,25 @@
 from django.shortcuts import render
 
 def index(request):
-    return render(request,'index.html')
+    receitas = {
+        1: 'Suco de Melão',
+        2: 'Pizza',
+        3: 'Suco de Melancia',
+        4: 'Suco de Abacaxi',
+        5: 'Suco de Maracujá',
+        6: 'Suco de Laranja',
+        7: 'Suco de Acerola',
+        8: 'Suco de Uva',
+        9: 'Suco de Maça',
+        10: 'Suco de Tamarino',
+
+        }
+
+    dados = {
+        'lista_receitas' : receitas
+        }
+
+    return render(request,'index.html',dados)
     
 def contato(request):
     return render(request,'contato.html')    
