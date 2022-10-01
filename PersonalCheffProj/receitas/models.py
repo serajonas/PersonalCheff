12c0ea4c0193035d39c010945ba5b276f0055efc
Jonas Serafim <104076718+serajonas@users.noreply.github.com>
2022-10-01 11:21:44 -0300

    Tabelas do banco de dados

--- PersonalCheffProj/receitas/models.py
+++ PersonalCheffProj/receitas/models.py
@@ -1,8 +1,7 @@
-from statistics import mode
 from django.db import models
 from datetime import datetime
 
-class Receitas(models,Model):
+class Receitas(models.Model):
     nome_receitas = models.CharField(max_length=180)
     video = models.CharField(max_length=88)
     modo_preparo = models.TextField()
