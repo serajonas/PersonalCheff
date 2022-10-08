6c3afb146bd4591601b1f8ace5057f81fd88230f
Jonas Serafim <104076718+serajonas@users.noreply.github.com>
2022-10-08 11:37:14 -0300

    Receitas Registros

--- PersonalCheffProj/receitas/admin.py
+++ PersonalCheffProj/receitas/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Receitas
+
+admin.site.register(Receitas)
 
 # Register your models here.
--- PersonalCheffProj/receitas/views.py
+++ PersonalCheffProj/receitas/views.py
@@ -1,19 +1,8 @@
 from django.shortcuts import render
+from .models import Receitas
 
 def index(request):
-    receitas = {
-        1: 'Suco de Melão',
-        2: 'Pizza',
-        3: 'Suco de Melancia',
-        4: 'Suco de Abacaxi',
-        5: 'Suco de Maracujá',
-        6: 'Suco de Laranja',
-        7: 'Suco de Acerola',
-        8: 'Suco de Uva',
-        9: 'Suco de Maça',
-        10: 'Suco de Tamarino',
-
-        }
+    receitas = Receitas.objects.all()
 
     dados = {
         'lista_receitas' : receitas
